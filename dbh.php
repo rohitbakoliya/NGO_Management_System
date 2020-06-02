5088e8d4bac58e58d0b2bcbd5f1cc57630c8ace1
rohitbakoliya <bakoliyarohit00@gmail.com>
2020-06-02 11:23:45 +0530

    initial commit

---
+++ dbh.php
@@ -0,0 +1,7 @@
+<?php
+ $dbServer = "localhost";
+ $dbUsername = "rohit";
+ $dbPassword = "test1234";
+ $dbName = "ngo_management";
+   
+ $conn = mysqli_connect($dbServer, $dbUsername, $dbPassword, $dbName);
---
+++ verify.php
@@ -0,0 +1,36 @@
+
+<?php
+session_start();
+  include_once $_SERVER['DOCUMENT_ROOT'] . "/program_files/dbh.php";
+  $username = $_POST['username'];
+  $_SESSION['name'] = $username;
+  $password = $_POST['password'];
+  $flag = 0;
+  // $check = 0;
+  $sql = "SELECT name,password FROM user_info;";
+  $result  = mysqli_query($conn,$sql);
+   while($row = mysqli_fetch_assoc($result)){
+       if($row['name'] == $username && $row['password'] == $password)
+       {
+               header('location: home.php');
+              //  $check =0;
+       }
+       else{
+           $flag = 1;
+       }
+   }
+?>
+<!DOCTYPE html>
+<html>
+  <head></head>
+  <body>
+  <!-- <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script> -->
+<script>
+   var check = "<?php echo $flag;?>";
+   if(check)
+   {
+    alert('Opps something wrong!\nLogin failed!!!\nPlease retry with correct credentials\n');
+   }
+ </script>
+ </body>
+ </html>
